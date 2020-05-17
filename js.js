c3fe9f274bed22f01caf61f8c61ada3215ff6ee1
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-05-17 15:52:55 +0700

    ứng dụng máy tính

---
+++ js.js
@@ -0,0 +1,26 @@
+function addition() {
+    let a = parseInt(document.getElementById("num1").value);
+    let b = parseInt(document.getElementById("num2").value);
+    let k = a+b;
+    document.getElementById("result").innerHTML = "Result Divition:" + k;
+}
+function subtraction() {
+    let a = parseInt(document.getElementById("num1").value);
+    let b = parseInt(document.getElementById("num2").value);
+    let k = a - b;
+    document.getElementById("result").innerHTML = "Result Divition:" + k;
+}
+function munTipLiCaTion() {
+    let a = parseInt(document.getElementById("num1").value);
+    let b = parseInt(document.getElementById("num2").value);
+    let k = a * b;
+    document.getElementById("result").innerHTML = "Result Divition:" + k;
+}
+function divISion() {
+    let a = parseInt(document.getElementById("num1").value);
+    let b = parseInt(document.getElementById("num2").value);
+    let k = a / b;
+    document.getElementById("result").innerHTML = "Result Divition:" + k;
+}
+
+
